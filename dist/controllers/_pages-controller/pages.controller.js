3c91c64cac87d965efcbdce8e8884e5185f696c7
K28497 <ryan.waite@finra.org>
2019-11-18 12:08:49 -0500

    removed dist/ folder

--- dist/controllers/_pages-controller/pages.controller.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express = require("express");
-const GET = require("./get.method");
-exports.PagesController = express.Router();
-exports.PagesController.get('/', GET.WelcomePage);
